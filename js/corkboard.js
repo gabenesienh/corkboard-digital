ffe03c90be3fb1f9e24c41c045ec7f77ad8ddd8b
gabenesienh <gabrielenes@ienh.com.br>
2025-12-04 09:35:58 -0300

    Implementar drag & drop
    Aprimorar CSS de post-its

--- js/corkboard.js
+++ js/corkboard.js
@@ -5,16 +5,25 @@ document.addEventListener("DOMContentLoaded", () => {
   // Usado para identificar individualmente os itens do quadro
   let novoItemId = 0;
 
-  for (const e of painelBotoes) {
-    e.addEventListener("click", () => {
+  // Ao arrastar (drag & drop) um item, isso guarda uma referência do item
+  let itemSegurado = null;
+
+  // O quão longe o mouse deve ficar do canto superior esquerdo do item segurado
+  // É definido ao clicar no item, e usado na hora de movê-lo
+  let itemSeguradoOffsetX = 0;
+  let itemSeguradoOffsetY = 0;
+
+  for (const btn of painelBotoes) {
+    btn.addEventListener("click", () => {
       const novoItem = document.createElement("div");
+      novoItem.classList.add("item");
 
       // Área superior do item (onde ficaria um alfinete, na vida real)
       const novoItemTop = document.createElement("div");
       novoItemTop.classList.add("item-top");
       novoItem.appendChild(novoItemTop);
 
-      switch (e.id) {
+      switch (btn.id) {
         // Post-it para texto
         case "btn-adicionar-post-it":
           novoItem.classList.add("post-it");
@@ -24,6 +33,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
           break;
       }
+
+      // Mover item para a frente do quadro
+      novoItem.style.zIndex = novoItemId;
 
       // Dar um ID ao item criado
       novoItem.id = "item-" + novoItemId;
@@ -45,6 +57,43 @@ document.addEventListener("DOMContentLoaded", () => {
       const randomY = Math.random()*(quadroRect.height - itemAltura);
       novoItem.style.left = `${randomX}px`;
       novoItem.style.top = `${randomY}px`;
+
+      // Detectar quando o topo do item é clicado, para permitir drag´ & drop
+      novoItemTop.addEventListener("mousedown", (e) => {
+        if (e.button !== 0) return;
+
+        novoItemTop.style.cursor = "grabbing";
+
+        itemSegurado = novoItem;
+        itemSeguradoOffsetX = e.clientX - parseInt(itemSegurado.style.left, 10);
+        itemSeguradoOffsetY = e.clientY - parseInt(itemSegurado.style.top, 10);
+        
+        // Mover item para a frente do quadro (movendo os outros para trás)
+        for (const item of document.getElementsByClassName("item")) {
+          if (item.style.zIndex > itemSegurado.style.zIndex) {
+            item.style.zIndex--;
+          }
+        }
+
+        itemSegurado.style.zIndex = novoItemId;
+      })
     });
   }
+
+  document.body.addEventListener("mousemove", (e) => {
+    if (e.button !== 0) return;
+    if (itemSegurado === null) return;
+
+    itemSegurado.style.left = `${e.clientX - itemSeguradoOffsetX}px`;
+    itemSegurado.style.top = `${e.clientY - itemSeguradoOffsetY}px`;
+  });
+
+  document.body.addEventListener("mouseup", (e) => {
+    if (e.button !== 0) return;
+    if (itemSegurado === null) return;
+
+    itemSegurado.querySelector(".item-top").style.cursor = "";
+
+    itemSegurado = null;
+  });
 });
